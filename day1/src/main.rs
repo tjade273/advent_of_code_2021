01a4f96aec3ea17c58bf4678a7980876f295d861
Tjaden Hess <tjade273@gmail.com>
2021-12-02 11:07:04 -0600

    Cleanup and formatting

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,7 +1,7 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn count_increase<'a>(depths: &[usize]) -> usize {
+fn count_increase(depths: &[usize]) -> usize {
     let pairs = depths.windows(2);
     pairs.filter(|pair| pair[1] > pair[0]).count()
 }
@@ -9,7 +9,10 @@ fn count_increase<'a>(depths: &[usize]) -> usize {
 fn read_ints(filename: &str) -> Vec<usize> {
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
-    reader.lines().map(|x| x.unwrap().parse().unwrap()).collect()
+    reader
+        .lines()
+        .map(|x| x.unwrap().parse().unwrap())
+        .collect()
 }
 
 fn puzzle1(input: &[usize]) {
@@ -17,7 +20,7 @@ fn puzzle1(input: &[usize]) {
 }
 
 fn puzzle2(input: &[usize]) {
-    let windows : Vec<usize> = input.windows(3).map(|window| window.iter().sum()).collect();
+    let windows: Vec<usize> = input.windows(3).map(|window| window.iter().sum()).collect();
     println!("{}", count_increase(&windows));
 }
 
--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,60 +1,121 @@
 use std::str::FromStr;
 
-enum Direction{
+#[derive(Copy, Clone)]
+enum Direction {
     Forward,
     Up,
-    Down
+    Down,
 }
 
+#[derive(Copy, Clone)]
 struct Command {
     direction: Direction,
-    distance: usize
+    distance: usize,
 }
 
-struct Position {
+trait Pilot {
+    fn up(&mut self, n: usize);
+    fn down(&mut self, n: usize);
+    fn forward(&mut self, n: usize);
+
+    fn execute(&mut self, command: Command) {
+        let Command {
+            direction,
+            distance,
+        } = command;
+        match direction {
+            Direction::Up => self.up(distance),
+            Direction::Down => self.down(distance),
+            Direction::Forward => self.forward(distance),
+        }
+    }
+}
+
+struct Position1 {
     x: usize,
     y: usize,
-    aim: usize
+}
+
+impl Position1 {
+    fn new() -> Self {
+        Position1 { x: 0, y: 0 }
+    }
+}
+
+impl Pilot for Position1 {
+    fn up(&mut self, n: usize) {
+        self.y -= n
+    }
+
+    fn down(&mut self, n: usize) {
+        self.y += n
+    }
+
+    fn forward(&mut self, n: usize) {
+        self.x += n
+    }
+}
+
+struct Position2 {
+    x: usize,
+    y: usize,
+    aim: usize,
+}
+
+impl Position2 {
+    fn new() -> Self {
+        Position2 { x: 0, y: 0, aim: 0 }
+    }
+}
+
+impl Pilot for Position2 {
+    fn up(&mut self, n: usize) {
+        self.aim -= n
+    }
+
+    fn down(&mut self, n: usize) {
+        self.aim += n
+    }
+
+    fn forward(&mut self, n: usize) {
+        self.x += n;
+        self.y += n * self.aim
+    }
 }
 
 impl FromStr for Command {
     type Err = String;
 
     fn from_str(cmd: &str) -> Result<Self, Self::Err> {
-        let (direction_str, distance_str) = cmd.split_once(' ').ok_or(format!("Malformed Command: {}", cmd))?;
-        let distance = distance_str.parse().or(Err(format!("Could not parse distance: {}", distance_str)))?;
+        let (direction_str, distance_str) = cmd
+            .split_once(' ')
+            .ok_or(format!("Malformed Command: {}", cmd))?;
+        let distance = distance_str
+            .parse()
+            .map_err(|_| format!("Could not parse distance: {}", distance_str))?;
         let direction = match direction_str {
             "forward" => Ok(Direction::Forward),
             "up" => Ok(Direction::Up),
             "down" => Ok(Direction::Down),
-            _ => Err(format!("Invalid Direction: {}", direction_str))
+            _ => Err(format!("Invalid Direction: {}", direction_str)),
         }?;
-        Ok(Command {direction, distance})
-    }
-}
-
-fn execute_command_part1(position: Position, command: Command) -> Position {
-    let Position {x, y, aim} = position;
-    match command.direction {
-        Direction::Forward => Position {x: x + command.distance, y, aim},
-        Direction::Up => Position {x, y: y - command.distance, aim},
-        Direction::Down => Position {x, y: y + command.distance, aim}
-    }
-}
-
-fn execute_command_part2(position: Position, command: Command) -> Position {
-    let Position {x, y, aim} = position;
-    match command.direction {
-        Direction::Forward => Position {x: x + command.distance, y: y + command.distance * aim, aim},
-        Direction::Up => Position {x, y, aim: aim - command.distance},
-        Direction::Down => Position {x, y, aim: aim + command.distance}
+        Ok(Command {
+            direction,
+            distance,
+        })
     }
 }
 
 fn main() {
     let input = include_str!("input.txt");
-    let Position {x, y, aim: _} = input.split_terminator('\n').map(|l| l.parse().unwrap()).fold(Position {x: 0, y: 0, aim: 0}, execute_command_part1);
-    println!("{}", x*y);
-    let Position {x, y, aim: _} = input.split_terminator('\n').map(|l| l.parse().unwrap()).fold(Position {x: 0, y: 0, aim: 0}, execute_command_part2);
-    println!("{}", x*y)
+    let mut pos1 = Position1::new();
+    let mut pos2 = Position2::new();
+    input
+        .split_terminator('\n')
+        .map(|l| l.parse().unwrap())
+        .for_each(|cmd| {
+            pos1.execute(cmd);
+            pos2.execute(cmd)
+        });
+    println!("{}, {}", pos1.x * pos1.y, pos2.x * pos2.y);
 }
