31c2155255252050bc440392dae4b6067cdc8b1b
Tjaden Hess <tjade273@gmail.com>
2021-12-03 14:24:01 -0600

    Day3 finished (rough)

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -9,17 +9,6 @@ fn column_majority(mat: &Vec<Vec<usize>>, col: usize) -> usize {
     (2 * mat.iter().map(|r| r[col]).sum::<usize>() >= mat.len()) as usize
 }
 
-fn transpose<T: Copy>(mat: &Vec<Vec<T>>) -> Vec<Vec<T>> {
-    assert!(!mat.is_empty() && !mat[0].is_empty());
-    assert!(mat.iter().all(|r| r.len() == mat[0].len()));
-
-    let nrows = mat.len();
-    let ncols = mat[0].len();
-    (0..ncols).map(|c| (0..nrows).map(|r| mat[r][c]).collect()).collect()
-}
-
-
-
 fn gamma_vec(bits: &Vec<Vec<usize>>) -> Vec<usize> {
     (0..bits[0].len()).map(|col| column_majority(bits, col)).collect()
 }
@@ -27,27 +16,29 @@ fn gamma_vec(bits: &Vec<Vec<usize>>) -> Vec<usize> {
 fn bits_to_int(bits: &Vec<usize>) -> usize {
     bits.iter().fold(0, |acc, b| (acc << 1) + b)
 }
-/*
-fn oxygen_rating(_bits: &Vec<Vec<usize>>) -> Vec<usize> {
-    let bits: Vec<&Vec<usize>>= _bits.iter().collect();
-    let ncols = bits[0].len(); 
-    for i in 0..ncols {
-        if bits.len() == 1 {
-            break
-        }
-        let gamma = gamma_vec(&bits)[i];
-        bits = bits.iter().filter(|r| r[i] == gamma).collect();
+
+
+fn gas_rating(_bits: &Vec<Vec<usize>>, negate: bool) -> Vec<usize> {
+    let mut bits = _bits.clone();
+    let mut col = 0;
+    while bits.len() > 1 {
+        let maj = column_majority(&bits, col);
+        bits = bits.iter().filter(|r| negate ^ (r[col] == maj)).cloned().collect();
+        col += 1
     }
-    return bits[0];
+    bits[0].clone()
 }
-*/
+
 fn main() {
     let input = include_str!("input.txt");
     let input_bits = parse_input(input);
     let gamma_vec = gamma_vec(&input_bits);
     let epsilon_vec = gamma_vec.iter().map(|b| (b + 1) & 1).collect();
     let power = bits_to_int(&gamma_vec) * bits_to_int(&epsilon_vec);
-    println!("{}", power)
+    let oxygen = gas_rating(&input_bits, true);
+    let co2 = gas_rating(&input_bits, false);
+    let status = bits_to_int(&oxygen) * bits_to_int(&co2);
+    println!("{}, {}", power, status)
 }
 
 #[cfg(test)]
@@ -61,8 +52,8 @@ mod tests {
     }
 
     #[test]
-    fn test_transpose() {
-        let v = vec![vec![0,0,0], vec![1,1,1]];
-        assert!(transpose(&v) == vec![vec![0,1], vec![0,1], vec![0,1]])
+    fn test_oxygen_rating() {
+        let input = include_str!("test.txt");
+        assert!(bits_to_int(&oxygen_rating(&parse_input(input), false)) == 23)
     }
 }
