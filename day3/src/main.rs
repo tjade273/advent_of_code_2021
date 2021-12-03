dbcd9779f12a4425a41d98385dfbe911ea2c6e4b
Tjaden Hess <tjade273@gmail.com>
2021-12-03 15:01:29 -0600

    Lints

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -5,22 +5,22 @@ fn parse_input(input: &str) -> Vec<Vec<usize>> {
         .collect()
 }
 
-fn column_majority(mat: &Vec<Vec<usize>>, col: usize) -> usize {
+fn column_majority(mat: &[Vec<usize>], col: usize) -> usize {
     (2 * mat.iter().map(|r| r[col]).sum::<usize>() >= mat.len()) as usize
 }
 
-fn gamma_vec(bits: &Vec<Vec<usize>>) -> Vec<usize> {
+fn gamma_vec(bits: &[Vec<usize>]) -> Vec<usize> {
     (0..bits[0].len())
         .map(|col| column_majority(bits, col))
         .collect()
 }
 
-fn bits_to_int(bits: &Vec<usize>) -> usize {
+fn bits_to_int(bits: &[usize]) -> usize {
     bits.iter().fold(0, |acc, b| (acc << 1) + b)
 }
 
-fn gas_rating(_bits: &Vec<Vec<usize>>, negate: bool) -> Vec<usize> {
-    let mut bits = _bits.clone();
+fn gas_rating(_bits: &[Vec<usize>], negate: bool) -> Vec<usize> {
+    let mut bits = _bits.to_owned();
     let mut col = 0;
     while bits.len() > 1 {
         let maj = column_majority(&bits, col);
@@ -38,7 +38,7 @@ fn main() {
     let input = include_str!("input.txt");
     let input_bits = parse_input(input);
     let gamma_vec = gamma_vec(&input_bits);
-    let epsilon_vec = gamma_vec.iter().map(|b| (b + 1) & 1).collect();
+    let epsilon_vec: Vec<usize> = gamma_vec.iter().map(|b| (b + 1) & 1).collect();
     let power = bits_to_int(&gamma_vec) * bits_to_int(&epsilon_vec);
     let oxygen = gas_rating(&input_bits, true);
     let co2 = gas_rating(&input_bits, false);
