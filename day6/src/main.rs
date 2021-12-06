489a0f1022ad69a822664858491a3037b2212e1e
Tjaden Hess <tjade273@gmail.com>
2021-12-06 15:46:38 -0600

    Day 6 Part 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,18 +1,15 @@
-
-fn count_from_fish(fish: impl IntoIterator<Item = usize>) -> [usize; 9] 
-{
-    let mut fish_count : [usize; 9] = [0; 9];
+fn count_from_fish(fish: impl IntoIterator<Item = usize>) -> [usize; 9] {
+    let mut fish_count: [usize; 9] = [0; 9];
     for f in fish {
-        fish_count[f] += 1; 
+        fish_count[f] += 1;
     }
     fish_count
 }
 
-fn advance(fish_count: &mut[usize; 9]) {
+fn advance(fish_count: &mut [usize; 9]) {
     fish_count.rotate_left(1);
     fish_count[6] += fish_count[8]
 }
-
 
 fn main() {
     let input = include_str!("input.txt")
@@ -23,6 +20,10 @@ fn main() {
 
     let mut fish_count = count_from_fish(input);
     for _ in 0..80 {
+        advance(&mut fish_count)
+    }
+    println!("{}", fish_count.iter().sum::<usize>());
+    for _ in 0..(256 - 80) {
         advance(&mut fish_count)
     }
     println!("{}", fish_count.iter().sum::<usize>())
